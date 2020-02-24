15feabc32a3dea4217443ecbcef90c6e71c5f354
W. Guilherme <ravok123@gmail.com>
2020-02-24 20:20:46 -0300

    Updates

---
+++ precache-manifest.0ec26f7fe11a159b246f62d6302a6796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b250e3f241650e918741d9e915a2ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3e39ea890c32ef1ecb",
+    "url": "/static/css/main.94f1c811.chunk.css"
+  },
+  {
+    "revision": "ac9a0c7732d6ed4f68ec",
+    "url": "/static/js/2.71b4934f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.71b4934f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3e39ea890c32ef1ecb",
+    "url": "/static/js/main.a06ca576.chunk.js"
+  },
+  {
+    "revision": "9f0a0101da083ff61f0f",
+    "url": "/static/js/runtime-main.58cf8ba4.js"
+  }
+]);
